e1b4ad7d1dfeb6e2ee7dd5c22588da417d8e6a73
Abhijith <abhijithpb2506@gmail.com>
2025-12-03 12:09:58 +0530

    form validation

--- src/components/IncomeTracking/IncomeTracking.jsx
+++ src/components/IncomeTracking/IncomeTracking.jsx
@@ -2,24 +2,29 @@ import './IncomeTracking.css';
 import '../FinanceTracker/FinanceTracker.css';
 function IncomeTracking({income,source,date,handleIncome,handleSource,handleDate,AddIncome})
 {
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    AddIncome();
+  }
   return(
     <>
-    
-    <div className="income-tracking-container">
+    <form id='income' onSubmit={handleSubmit}>
+    <div  className="income-tracking-container">
            <h2 className='income-title'>INCOME TRACKING</h2>
            <label>Amount</label>
-           <input className="income-catcher"  value={income} type="text" onChange={handleIncome}/>
+           <input className="income-catcher" required  value={income} type="text" onChange={handleIncome}/>
            <label>Source of income</label>
-           <select className='source-catcher' value={source} onChange={handleSource}>
-            <option value="">Select Source</option>
+           <select className='source-catcher' value={source} onChange={handleSource} required>
+            <option value="" >Select Source</option>
              <option>Salary</option>
              <option>Side hustle</option>
              <option>Investments</option>
            </select>
            <label>Date</label>
-           <input className='date-catcher' value={date} type="date" onChange={handleDate}/>
-           <button className='income-add-button' onClick={AddIncome}>Add</button>
+           <input className='date-catcher' value={date} type="date" onChange={handleDate} required/>
+           <button type='submit' className='income-add-button'>Add</button>
         </div>
+        </form>
          </>
   );
 }
